d1c7f034e9b71091e73242d94942986d12083eaf
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-05-17 11:55:44 +0700

    fix: update temporary image

--- resources/views/livewire/admin/product/edit.blade.php
+++ resources/views/livewire/admin/product/edit.blade.php
@@ -2,7 +2,7 @@
     <div>
         <div class="app-card shadow-sm mb-4 p-4">
             @if (Session::has('msg'))
-                <div class="alert alert-success" role="alert">{{ Session::get('msg') }}</div>
+            <div class="alert alert-success" role="alert">{{ Session::get('msg') }}</div>
             @endif
             <h3>Edit Barang</h3>
             <hr>
@@ -12,132 +12,116 @@
                 <div class="row">
                     <div class="form-outline mb-4">
                         <label class="form-label" for="loginName">Nama Barang</label>
-                        <input type="text" class="form-control @error('name') is-invalid @enderror" wire:model="name"
-                            name="name" value="{{ old('name') }}" required autocomplete="name" autofocus
-                            wire:keyup="generateSlug" />
+                        <input type="text" class="form-control @error('name') is-invalid @enderror" wire:model="name" name="name" value="{{ old('name') }}" required autofocus wire:keyup="generateSlug" />
                         @error('name')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
                     {{-- slug --}}
                     <div class="form-outline mb-4">
                         <label class="form-label" for="loginName">Slug</label>
-                        <input type="text" name="slug" id="slug"
-                            class="form-control @error('slug')is-invalid @enderror" wire:model="slug"
-                            value="{{ old('slug') }}" />
+                        <input type="text" name="slug" id="slug" class="form-control @error('slug')is-invalid @enderror" wire:model="slug" value="{{ old('slug') }}" />
 
                         @error('slug')
-                            <span class="invalid-feedback">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                        <span class="invalid-feedback">
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
                     {{-- anime --}}
                     <div class="form-outline mb-4">
                         <label class="form-label" for="loginName">Anime Series</label>
-                        <input type="text" name="anime" id="anime"
-                            class="form-control @error('anime')is-invalid @enderror" wire:model="anime"
-                            value="{{ old('anime') }}" />
+                        <input type="text" name="anime" id="anime" class="form-control @error('anime')is-invalid @enderror" wire:model="anime" value="{{ old('anime') }}" />
 
                         @error('anime')
-                            <span class="invalid-feedback">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                        <span class="invalid-feedback">
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
                     {{-- Kode --}}
                     <div class="form-outline mb-4">
                         <label class="form-label" for="loginName">Kode Barang</label>
-                        <input type="text" name="kode" id="kode"
-                            class="form-control @error('kode')is-invalid @enderror" wire:model="kode"
-                            value="{{ old('kode') }}" aria-describedby="emailHelp" />
+                        <input type="text" name="kode" id="kode" class="form-control @error('kode')is-invalid @enderror" wire:model="kode" value="{{ old('kode') }}" aria-describedby="emailHelp" />
                         <div id="emailHelp" class="form-text">Example: KD400.</div>
 
                         @error('kode')
-                            <span class="invalid-feedback">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                        <span class="invalid-feedback">
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
                     {{-- Deskripsi --}}
                     <div class="form-outline mb-4">
                         <label class="form-label" for="loginName">Deskripsi Barang</label>
-                        <input type="text" name="desc" id="desc"
-                            class="form-control @error('desc')is-invalid @enderror" wire:model="desc"
-                            value="{{ old('desc') }}" />
+                        <input type="text" name="desc" id="desc" class="form-control @error('desc')is-invalid @enderror" wire:model="desc" value="{{ old('desc') }}" />
 
                         @error('desc')
-                            <span class="invalid-feedback">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                        <span class="invalid-feedback">
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
                     {{-- Regular price --}}
                     <div class="form-outline mb-4">
                         <label class="form-label" for="loginName">Harga Barang</label>
-                        <input type="number" name="regular_price" id="regular_price"
-                            class="form-control @error('regular_price')is-invalid @enderror" wire:model="regular_price"
-                            value="{{ old('regular_price') }}" />
+                        <input type="number" name="regular_price" id="regular_price" class="form-control @error('regular_price')is-invalid @enderror" wire:model="regular_price" value="{{ old('regular_price') }}" />
 
                         @error('regular_price')
-                            <span class="invalid-feedback">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                        <span class="invalid-feedback">
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
                     {{-- sale price --}}
                     <div class="form-outline mb-4">
                         <label class="form-label" for="loginName">Harga Jual</label>
-                        <input type="number" name="harga_jual" id="harga_jual"
-                            class="form-control @error('harga_jual')is-invalid @enderror" wire:model="harga_jual"
-                            value="{{ old('harga_jual') }}" />
+                        <input type="number" name="harga_jual" id="harga_jual" class="form-control @error('harga_jual')is-invalid @enderror" wire:model="harga_jual" value="{{ old('harga_jual') }}" />
 
                         @error('harga_jual')
-                            <span class="invalid-feedback">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                        <span class="invalid-feedback">
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
                     {{-- quantity --}}
                     <div class="form-outline mb-4">
                         <label class="form-label" for="loginName">Jumlah Barang</label>
-                        <input type="number" name="quantity" id="quantity"
-                            class="form-control @error('quantity')is-invalid @enderror" wire:model="quantity"
-                            value="{{ old('quantity') }}" />
+                        <input type="number" name="quantity" id="quantity" class="form-control @error('quantity')is-invalid @enderror" wire:model="quantity" value="{{ old('quantity') }}" />
 
                         @error('quantity')
-                            <span class="invalid-feedback">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                        <span class="invalid-feedback">
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
                     {{-- image --}}
                     <div class="form-outline mb-4">
                         <label class="form-label" for="loginName">Gambar Barang</label>
-                        <input type="file" name="gambar" id="" class="form-control"
-                            wire:model="new_image" />
+                        <input type="file" name="gambar" id="" class="form-control" wire:model="new_image" />
                         @if ($new_image)
-                            <img src="{{ $gambar->temporaryUrl() }}" alt="" width="120">
+                        <img src="{{ $new_image->temporaryUrl() }}" alt="" width="120">
                         @else
-                            <img src="{{ asset('checkout/product/') }}/{{ $gambar }}" alt="" width="120">
+                        <img src="{{ asset('checkout/product/') }}/{{ $gambar }}" alt="" width="120">
                         @endif
 
                         @error('new_image')
-                            <span class="invalid-feedback">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                        <span class="invalid-feedback">
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
+
 
                     {{-- Stock Status --}}
                     <div class="form-outline mb-4">
@@ -150,9 +134,9 @@
                         </select>
 
                         @error('stock_status')
-                            <span class="invalid-feedback">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                        <span class="invalid-feedback">
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
@@ -167,9 +151,9 @@
                         </select>
 
                         @error('harga_jual')
-                            <span class="invalid-feedback">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                        <span class="invalid-feedback">
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
@@ -180,14 +164,14 @@
                         <select name="kategori_id" id="kategori_id" class="form-control" wire:model="kategori_id">
                             <option value="">-- Select Kategori --</option>
                             @foreach ($kategori as $x)
-                                <option value="{{ $x->id }}">{{ $x->name }}</option>
+                            <option value="{{ $x->id }}">{{ $x->name }}</option>
                             @endforeach
                         </select>
 
                         @error('harga_jual')
-                            <span class="invalid-feedback">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                        <span class="invalid-feedback">
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
                     {{-- submit --}}
