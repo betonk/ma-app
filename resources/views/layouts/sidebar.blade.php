5c6567b434a96b267f4c8a142a73b1991a53ea87
betonk26 <betonk26@gmail.com>
2023-05-20 21:53:33 +0700

    fixing sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -267,9 +267,9 @@
                                 <li class="submenu-item"><a
                                         class="submenu-link {{ Request::is('admin/kategori') ? 'active' : '' }}"
                                         href="{{ route('kategori.admin') }}">Kategori</a></li>
-                                        <li class="submenu-item"><a
-                                            class="submenu-link {{ Request::is('admin/member') ? 'active' : '' }}"
-                                            href="{{ route('member.admin') }}">Member</a></li>
+                                <li class="submenu-item"><a
+                                        class="submenu-link {{ Request::is('admin/member') ? 'active' : '' }}"
+                                        href="{{ route('member.admin') }}">Member</a></li>
 
                             </ul>
                         </div>
@@ -431,20 +431,15 @@
                         </li> --}}
                         <!--//nav-item-->
                         <li class="nav-item">
-                            <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
-                            <a class="nav-link"
-                                href="https://themes.3rdwavemedia.com/bootstrap-templates/admin-dashboard/portal-free-bootstrap-admin-dashboard-template-for-developers/">
-                                {{-- <span class="nav-icon">
-                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-person"
-                                        fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd"
-                                            d="M12 1H4a1 1 0 0 0-1 1v10.755S4 11 8 11s5 1.755 5 1.755V2a1 1 0 0 0-1-1zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z" />
-                                        <path fill-rule="evenodd" d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                                    </svg>
-                                </span> --}}
-                                <span class="nav-link-text">Logout</span>
-                            </a>
-                            <!--//nav-link-->
+                            <a class="nav-link" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                        @csrf
+                                    </form>
                         </li>
                         <!--//nav-item-->
                     </ul>
--- resources/views/livewire/admin/preorder/index.blade.php
+++ resources/views/livewire/admin/preorder/index.blade.php
@@ -10,14 +10,15 @@
                 <h1 class="app-page-title">&nbsp; Data Pre Order</h1>
             </div>
             <div class="col-6 text-end">
-                <a href="{{ route('generate-pdf.po') }}" class="btn btn-warning"><i class="fa-solid fa-file-arrow-down"></i>&nbsp;PDF</a>
+                <a href="{{ route('generate-pdf.po') }}" class="btn btn-warning"><i
+                        class="fa-solid fa-file-arrow-down"></i>&nbsp;PDF</a>
             </div>
         </div>
         {{-- datatables --}}
         <table class="table table-bordered table-striped text-center align-items-center">
             <thead>
                 <tr>
-                    <th>No</th> 
+                    <th>No</th>
                     <th>Nama</th>
                     <th>Status</th>
                     <th>tgl order</th>
@@ -41,18 +42,16 @@
                         </td>
                         <td>{{ date_format($item->created_at, 'd-m-Y') }}</td>
                         <td class="d-flex justify-content-center">
-                            {{-- <a href="{{ url('barang/' . $item->id . '/edit') }}" class="text-primary"><i class="fas fa-fw fa-pen-to-square"></i></a> --}}
-                            {{-- <a href="" class="btn btn-danger">delete</a> --}}
+                            <a href="{{ route('product.edit', ['item_id' => $item->id]) }}" class="btn text-primary"><i
+                                    class="fas fa-fw fa-pen-to-square"></i></a>
                             |
-                            {{-- <form action="{{ url('admin/' . $item->id) }}" method="post">
-                                @csrf
-                                @method('delete')
-                                <button type="submit" class="text-primary border-0"><i
-                                        class="fas fa-fw fa-trash"></i></button>
-                            </form> --}}
+                            <button type="button" class="btn text-primary border-0"
+                                onclick="confirmDelete('{{ $item->id }}', '{{ $item->name }}')">
+                                <i class="fas fa-fw fa-trash"></i>
+                            </button>
                             |
-                            {{-- <button type="button" class="text-primary border-0" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal-{{ $item->id }}"><i class="fas fa-eye"></i></button> --}}
+                            <a class="btn text-primary border-0" data-bs-toggle="modal"
+                                data-bs-target="#exampleModal-{{ $item->id }}"><i class="fas fa-eye"></i></a>
                         </td>
                     </tr>
                 @endforeach
