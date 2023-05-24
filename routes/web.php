d43e89ec7254094d783fd52c1152a8c1e6db6d05
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-05-24 10:59:51 +0700

    feat: profile function and components

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\staff\StaffController;
 use App\Http\Controllers\user\UserController;
 use App\Http\Livewire\Details;
 use App\Http\Livewire\Keranjang;
+use App\Http\Livewire\Profile;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -26,7 +27,8 @@ use Illuminate\Support\Facades\Route;
 //     return view('frontend.first', ['title'=>'Marahobina Store']);
 // });
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/product/{slug}',Details::class)->name('product.details');
+Route::get('/product/{slug}', Details::class)->name('product.details');
+Route::get('/profile', Profile::class)->name('profile');
 // Route::get('/cart', Keranjang::class)->name('product.cart');
 Auth::routes();
 
@@ -43,12 +45,12 @@ Route::middleware(['auth', 'user-access:staff'])->group(function () {
 Route::middleware(['auth', 'user-access:admin'])->group(function () {
     // Product
     Route::get('/admin/product', [AdminController::class, 'index'])->name('home.admin');
-    Route::get('/admin/product-add',[ProductController::class, 'index'])->name('product.add');
-    Route::get('/admin/product-edit/{item_id}',[ProductController::class, 'edited'])->name('product.edit');
+    Route::get('/admin/product-add', [ProductController::class, 'index'])->name('product.add');
+    Route::get('/admin/product-edit/{item_id}', [ProductController::class, 'edited'])->name('product.edit');
 
     //Member
     Route::get('/admin/member', [AdminController::class, 'member_view'])->name('member.admin');
-    
+
     // Kategori
     Route::get('/admin/kategori', [AdminController::class, 'kategori_view'])->name('kategori.admin');
 
@@ -59,6 +61,6 @@ Route::middleware(['auth', 'user-access:admin'])->group(function () {
     // Request Order
     Route::get('/admin/request', [AdminController::class, 'request_view'])->name('request.admin');
     Route::get('/admin/request/add', [RequestController::class, 'add'])->name('request.add');
-    Route::get('/admin/request-edit/{reqorder_id}',[RequestController::class, 'edited'])->name('request.edit');
+    Route::get('/admin/request-edit/{reqorder_id}', [RequestController::class, 'edited'])->name('request.edit');
     Route::get('/generate-pdf-ro', [AdminController::class, 'generate_pdf_ro'])->name('generate-pdf.ro');
 });
