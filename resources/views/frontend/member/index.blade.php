cbd66fdf7237e532f1937d2ba00dd0eb3e8992e2
betonk26 <betonk26@gmail.com>
2023-06-13 01:59:26 +0700

    request order done!

--- resources/views/frontend/member/index.blade.php
+++ resources/views/frontend/member/index.blade.php
@@ -6,7 +6,7 @@
     <body>
         <div class="container py-2">
             {{-- main content --}}
-            <div class="field p-4">
+            <div class="field p-4 mb-5">
                 <div class="row mt-4">
                     <div class="col-6 align-self-center">
                         <h1><b>The Most Complete Place To Meet Hobbyist Needs</b></h1>
@@ -16,9 +16,34 @@
                 </div>
             </div>
 
+            {{-- section regquest order --}}
+            <section>
+                <div class="field p-4">
+                    <div class="row">
+                        <div class="col-6">
+                            <h1 class="fw-bold text-primary">Request Order Item</h1>
+                            <div class="row g-4">
+                                <div class="col-10">
+                                    <input type="text" class="form-control"
+                                        placeholder="please, click button for request order!" />
+                                </div>
+                                <div class="col-auto">
+                                    <a href="http://wa.me/6282257627192?text=FORM%20REQUEST%20ORDER%0A%0ANama%20Pelanggan%3A%20%0ANo.%20Telp%20%2F%20WA%20%20%3A%20%0AAlamat%20Pengiriman%20%3A%20%0AJenis%20Pemesanan%20%5BPre-Order%2FRequest-Order%5D%20%3A%20%0A%0ANama%20Barang%20%3A%20%0AHarga%20%3A%0ALink%20%3A%20%0AJumlah%20%3A%0A%0AMetode%20Pembayaran%20%5BGopay%2FBCA%5D%3A%206801038643%20a.n%20AFN%0A%0A*Harap%20Diisi%20Dengan%20Benar%20untuk%20memudahkan%20selama%20proses%20pemesanan*%0A%0AThank%20You!!%20%E2%9C%8C%F0%9F%8F%BB"
+                                        class="btn btn-primary" target="blank">Order</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <h1 class="fw-bold text-primary">Shop with Easy!</h1>
+                            <p class="fw-bold">Now you can get target item form Japan, just by click button and send message to admin Marahobina!</p>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
             {{-- section catagories --}}
             <section id="kategori" class="my-5">
-
                 <h1 class="text-center text-primary fw-bold mb-5">Categories</h1>
 
                 <div class="row row-cols-1 row-cols-md-3 g-3 justify-content-between">
@@ -32,8 +57,6 @@
                         <a href=""><img src="{{ asset('./img/kt-3.svg') }}" class="card-img-top" alt=""></a>
                     </div>
                 </div>
-
-
             </section>
 
             {{-- section req order --}}
@@ -55,9 +78,8 @@
                     <div class="flex my-2">
                         <a href="https://instagram.com/marahobinashop" target="blank" class="text-decoration-none"><i
                                 class='ri-instagram-line' style="font-size: 1.5rem"></i></a>
-                        <a href="http://wa.me/6282257627192?text=FORM%20REQUEST%20ORDER%0A%0ANama%20Pelanggan%3A%20%0ANo.%20Telp%20%2F%20WA%20%20%3A%20%0AAlamat%20Pengiriman%20%3A%20%0AJenis%20Pemesanan%20%5BPre-Order%2FRequest-Order%5D%20%3A%20%0A%0ANama%20Barang%20%3A%20%0AHarga%20%3A%0ALink%20%3A%20%0AJumlah%20%3A%0A%0AMetode%20Pembayaran%20%5BGopay%2FBCA%5D%3A%206801038643%20a.n%20AFN%0A%0A*Harap%20Diisi%20Dengan%20Benar%20untuk%20memudahkan%20selama%20proses%20pemesanan*%0A%0AThank%20You!!%20%E2%9C%8C%F0%9F%8F%BB"
-                            target="blank" class="text-decoration-none"><i class='ri-whatsapp-line'
-                                style="font-size: 1.5rem"></i></a>
+                        <a href="http://wa.me/6282257627192?text=Hai%20Admin%20Marahobina!" target="blank"
+                            class="text-decoration-none"><i class='ri-whatsapp-line' style="font-size: 1.5rem"></i></a>
                     </div>
                     <p class="text-white">copyright &copy; {{ date('Y') }} Marahobina</p>
 
